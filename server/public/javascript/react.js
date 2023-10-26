c61e3afd958733ea7b2a946ca89af075f9acc072
Samuel <Samuel@Samuels-MacBook-2.local>
2023-10-26 15:09:32 -0500

    got clicking and setstate in react to work

--- server/public/javascript/react.js
+++ server/public/javascript/react.js
@@ -1,13 +1,27 @@
 console.log("running react.js");
 
+const ce = React.createElement
+
+function StatelessHello(props){
+  return ce('div', null, `Hello ${props.toWhat}`)
+}
 
 class Hello extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = { clickCount: 0 };
+  }
     render() {
-      return React.createElement('div', null, `Hello ${this.props.toWhat}`);
+      return ce('div', {onClick: (e) => this.clickHandler(e)}, `Hello ${this.props.toWhat} - click count ${this.state.clickCount}` );
+    }
+    
+    clickHandler(){
+      this.setState({clickCount: this.state.clickCount +1});
     }
   }
   
   ReactDOM.render(
-    React.createElement(Hello, {toWhat: 'World'}, null),
+    ce('div', null, ce(StatelessHello, {toWhat: 'Web Apps'}), ce(Hello, {toWhat: 'Web Apps'}, null)),
     document.getElementById('react-root')
   );
